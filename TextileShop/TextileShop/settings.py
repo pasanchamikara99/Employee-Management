94c44420962b5890f3b8cb204249ba61b55bd16d
Pasan Chamikara <pasanchamikara989@gmail.com>
2022-09-05 12:44:08 +0530

    add database config

--- TextileShop/TextileShop/settings.py
+++ TextileShop/TextileShop/settings.py
@@ -73,13 +73,19 @@ WSGI_APPLICATION = 'TextileShop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'testdatabase',  
+        'USER': 'root',  
+        'PASSWORD': '990108',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
+}  
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
