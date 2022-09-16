d489a27ab9e4292abc3e71c50401976aee5fca05
Pasan Chamikara <pasanchamikara989@gmail.com>
2022-09-16 08:34:03 +0530

    update user complete

--- TextileShop/authentication/views.py
+++ TextileShop/authentication/views.py
@@ -1,12 +1,23 @@
 from contextlib import redirect_stderr
 from http.client import HTTPResponse
 from pickle import NONE
+from re import template
+from urllib import response
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib import messages
 from authentication.models import EmployeesReg,EmployeeLeave
 from cryptography.fernet import Fernet
 import smtplib
 from django.contrib.auth.hashers import check_password,make_password
+from django.http import HttpResponse,FileResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
+
 
 
 
@@ -88,6 +99,7 @@ def login(request):
                 empid = request.POST.get('empid',None)
                 context['empid'] = empid
                 context['fname'] = emp.fname
+                
 
                 if emp.position == "admin" :
                     messages.success(request,"Admin login sucessfully")
@@ -105,13 +117,18 @@ def login(request):
 
 
 def adminpage(request):
-    return render(request,"admin.html",context)
+
+    result = EmployeesReg.objects.all() 
+    employee = {
+        "details" :result
+    }
+
+    return render(request,"admin.html",employee)
 
 def userpage(request):
     return render(request,"user.html",context)
 
 def changepassword(request):
-
     if request.method == "POST":
         empID = request.POST.get('empid')
         password = request.POST.get('password')
@@ -135,32 +152,7 @@ def changepassword(request):
                     messages.success(request,"Change password sucessfully")
                     return  redirect("userpage")
 
-
-
-            
-
-
-            
-    
-
-
-            
-
-
-
-
-    
-
-
-
     return render(request,"changepassword.html",context)
-
-    
-
-        
-        
-
-
 
     return render(request,"changepassword.html",context)
 
@@ -181,6 +173,83 @@ def applyleave(request):
 
 
     return render(request,"applyleave.html",context)
+
+
+def generatepdf(request):
+
+    buf = io.BytesIO()
+    c = canvas.Canvas(buf,pagesize=letter,bottomup=0)
+    textob = c.beginText()
+    textob.setTextOrigin(inch,inch)
+    textob.setFont("Helvetica",14)
+
+
+    result = EmployeesReg.objects.all()
+
+    lines = []
+
+    for emp in result:
+       lines.append("Employee ID : " + emp.empid)
+       lines.append("Employee First Name : " + emp.fname)
+       lines.append("Employee Last Name : " + emp.lname)
+       lines.append("Employee Email : " + emp.email)
+       lines.append("Employee Position : " + emp.position)
+       lines.append("====================")
+
+    for line in lines:
+        textob.textLine(line)
+
+    c.drawText(textob)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+
+
+    return FileResponse(buf,as_attachment=True,filename='employee.pdf')
+
+
+def update_emp(request,id):
+    employee = EmployeesReg.objects.get(id = id)
+    context['id'] = employee.id
+    context['empid'] = employee.empid
+    context['fname'] = employee.fname
+    context['lname'] = employee.lname
+    context['email'] = employee.email
+    context['position'] = employee.position
+    return render(request,"updateuser.html",context)
+
+def updateuser(request):
+    if request.method == "POST":
+        empID = request.POST['empid']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        position = request.POST['position']
+
+        result = EmployeesReg.objects.filter(empid=empID)   
+        for re in result:
+            saveRecord = EmployeesReg()
+            saveRecord.id = re.id
+            saveRecord.empid = empID
+            saveRecord.fname = fname
+            saveRecord.lname = lname
+            saveRecord.email = email
+            saveRecord.position = position
+            saveRecord.password = re.password
+            saveRecord.save()
+            messages.success(request,"Update details sucessfully")
+
+    return  redirect("adminpage")
+
+
+def delete_emp(request,id):
+
+    print(id)
+    employee = EmployeesReg.objects.get(id = id)
+
+    
+
+
 
 
 
